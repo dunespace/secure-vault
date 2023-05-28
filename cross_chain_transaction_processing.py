bba28c4a3928dcb66bcc8319b054622ec5e14511
dunespace <dunespace@icloud.com>
2023-05-28 14:00:01 +0200

    Update testing framework for Solidity

--- cross_chain_transaction_processing.py
+++ cross_chain_transaction_processing.py
@@ -1,3 +1,4 @@
 // Add support for batch transaction processing
 // Integrate privacy-preserving mechanisms
 // Integrate with external blockchain networks
+// Fix issue with multi-chain transactions
