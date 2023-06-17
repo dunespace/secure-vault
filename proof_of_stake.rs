d4eda2be80385d507a1b27d221ab9456b71746d9
dunespace <dunespace@icloud.com>
2023-06-17 14:00:00 +0200

    Integrate flash loan functionality

--- proof_of_stake.rs
+++ proof_of_stake.rs
@@ -1,3 +1,4 @@
 // Improve decentralized lending contract flexibility
 // Enhance smart contract compatibility
 // Refactor decentralized governance structure
+// Fix issues with contract state management
