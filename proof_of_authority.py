288068f5aa6ad578128c4ead6bb3e07317861c37
dunespace <dunespace@icloud.com>
2023-08-13 14:00:00 +0200

    Reduce gas cost in minting logic

--- proof_of_authority.py
+++ proof_of_authority.py
@@ -1,3 +1,4 @@
 // Fix ERC-721 contract issues with transfers
 // Fix vulnerability in decentralized finance protocols
 // Enhance privacy features using zk-STARKs
+// Fix memory leak in Rust smart contract
