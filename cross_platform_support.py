328d6c48f746134f301f78fd6b87321679b85b11
dunespace <dunespace@icloud.com>
2023-09-19 14:00:00 +0200

    Enhance smart contract upgradeability

--- cross_platform_support.py
+++ cross_platform_support.py
@@ -2,3 +2,4 @@
 // Fix interoperability issues with other blockchains
 // Fix transaction replay issue
 // Fix errors in contract interaction with wallets
+// Fix bugs in decentralized exchange routing
