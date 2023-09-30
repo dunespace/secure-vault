971ddb76f0031b1802d9f844943283f27e8658ad
dunespace <dunespace@icloud.com>
2023-09-30 14:00:00 +0200

    Refactor staking logic

--- gas_optimizer.rs
+++ gas_optimizer.rs
@@ -1,3 +1,4 @@
 // Implement on-chain identity verification
 // Optimize gas cost in function execution
 // Optimize gas cost in function execution
+// Integrate decentralized oracles for data feeds
