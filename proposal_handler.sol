7bde857a961b4feb4369bab578053979bbe10c6c
dunespace <dunespace@icloud.com>
2023-10-26 14:00:00 +0200

    Add automated token buyback mechanism

--- proposal_handler.sol
+++ proposal_handler.sol
@@ -1,3 +1,4 @@
 // Add support for decentralized file storage
 // Refactor contract to use EIP-712 signatures
 // Fix bugs in decentralized exchange routing
+// Fix issues with token transfer confirmations
