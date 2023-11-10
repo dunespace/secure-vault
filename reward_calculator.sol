6b01bbe6284b2a383dab61b8d94f638cda095ecb
dunespace <dunespace@icloud.com>
2023-11-10 14:00:01 +0200

    Fix bugs in NFT metadata handling

--- reward_calculator.sol
+++ reward_calculator.sol
@@ -1,3 +1,4 @@
 // Add support for multiple consensus mechanisms
 // Fix issues with contract state management
 // Optimize decentralized application performance
+// Fix issues in token transfer logic
