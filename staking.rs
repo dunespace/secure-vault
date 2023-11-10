9e575a04355046d4bb560f81742ac795a0d9c834
dunespace <dunespace@icloud.com>
2023-11-10 14:00:00 +0200

    Enhance API rate limiting for smart contracts

--- staking.rs
+++ staking.rs
@@ -3,3 +3,4 @@
 // Fix governance proposal approval logic
 // Improve decentralized exchange (DEX) liquidity
 // Add gas optimization features for smart contracts
+// Add support for delegation in governance
