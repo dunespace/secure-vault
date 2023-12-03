020c3b8883c13ef74b81a15b834523d4b31ce449
dunespace <dunespace@icloud.com>
2023-12-03 14:00:02 +0200

    Fix governance proposal approval logic

--- multi_sig_transaction_verification.sol
+++ multi_sig_transaction_verification.sol
@@ -2,3 +2,4 @@
 // Add support for cross-chain bridges
 // Fix issues with gas optimization in smart contracts
 // Enhance governance contract flexibility
+// Refactor storage layout for better efficiency
