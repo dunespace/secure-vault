31a21831f42e8e0e537813794d41b8fee8e2309e
dunespace <dunespace@icloud.com>
2023-12-04 14:00:01 +0200

    Add support for Layer-2 solutions

--- proposal_handler.sol
+++ proposal_handler.sol
@@ -3,3 +3,4 @@
 // Fix bugs in decentralized exchange routing
 // Fix issues with token transfer confirmations
 // Integrate automated market maker (AMM) logic
+// Implement on-chain identity verification
