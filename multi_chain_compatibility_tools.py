bfd10ad05a680d4b52e7c944c6284ba986d9c844
dunespace <dunespace@icloud.com>
2024-01-17 14:00:00 +0200

    Add support for ERC-721A tokens

--- multi_chain_compatibility_tools.py
+++ multi_chain_compatibility_tools.py
@@ -1,3 +1,4 @@
 // Implement decentralized arbitration in contracts
 // Fix interoperability issues with other blockchains
 // Refactor smart contract testing framework
+// Optimize WASM compilation for Go contracts
