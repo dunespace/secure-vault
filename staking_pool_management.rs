62f2000f19d2bf3318b345b9283dffee3ebf5bdf
dunespace <dunespace@icloud.com>
2024-01-29 14:00:01 +0200

    Improve token minting flow efficiency

--- staking_pool_management.rs
+++ staking_pool_management.rs
@@ -1,3 +1,4 @@
 // Fix governance proposal approval logic
 // Add support for decentralized lending platforms
 // Fix issues with cross-chain transaction finality
+// Fix issues with token transfer confirmations
