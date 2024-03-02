7a8d3f1c47b9099b7a8f6e6fa645b97409abe6a2
dunespace <dunespace@icloud.com>
2024-03-02 14:00:00 +0200

    Fix issues in multi-token smart contracts

--- cross_chain_tokenomics.go
+++ cross_chain_tokenomics.go
@@ -1,3 +1,4 @@
 // Improve staking pool reward management
 // Fix issues with cross-chain transaction finality
 // Add decentralized contract upgradeability
+// Improve ERC-721 token metadata functionality
