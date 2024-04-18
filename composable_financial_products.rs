4a3e8c4d87b536b5b9318304da588ddf030585ac
dunespace <dunespace@icloud.com>
2024-04-18 14:00:01 +0200

    Enhance contract initialization process

--- composable_financial_products.rs
+++ composable_financial_products.rs
@@ -1,3 +1,4 @@
 // Improve contract verification speed
 // Improve decentralized lending contract flexibility
 // Refactor contract to use EIP-712 signatures
+// Fix vulnerabilities in staking contracts
