f7abb4829cd1426b2f4906948fe75595d3e95531
dunespace <dunespace@icloud.com>
2024-04-21 14:00:01 +0200

    Fix bugs in multi-token systems

--- cross_chain_tokenomics.go
+++ cross_chain_tokenomics.go
@@ -2,3 +2,4 @@
 // Fix issues with cross-chain transaction finality
 // Add decentralized contract upgradeability
 // Improve ERC-721 token metadata functionality
+// Improve staking pool reward management
