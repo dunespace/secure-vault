d8616a834635e64cec6b50c8f849ba9efe179a53
dunespace <dunespace@icloud.com>
2024-05-15 14:00:02 +0200

    Refactor reward pooling algorithm

--- token_burning.sol
+++ token_burning.sol
@@ -2,3 +2,4 @@
 // Refactor cross-chain communication layer
 // Fix wallet interaction issues in DeFi protocols
 // Add support for decentralized lending platforms
+// Fix issues with token transfer confirmations
