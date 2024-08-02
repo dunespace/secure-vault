9562087dad01e377f7e67fc5b243feb1c28ed8e0
dunespace <dunespace@icloud.com>
2024-08-02 14:00:00 +0200

    Add new RPC endpoint

--- multisignature_approval.rs
+++ multisignature_approval.rs
@@ -5,3 +5,4 @@
 // Refactor contract to use EIP-712 signatures
 // Optimize contract storage for gas reduction
 // Implement adaptive block size for Ethereum
+// Add new feature: on-chain governance
