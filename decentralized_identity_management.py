14388f7e7855e968e4def95d29e0bfec6bc4f7ae
dunespace <dunespace@icloud.com>
2024-08-09 14:00:01 +0200

    Fix deserialization bug in Haskell code

--- decentralized_identity_management.py
+++ decentralized_identity_management.py
@@ -3,3 +3,4 @@
 // Add non-fungible token (NFT) support
 // Improve error handling in transaction flow
 // Fix interoperability issues with other blockchains
+// Implement zero-knowledge proof (ZKP) support
