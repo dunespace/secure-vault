692b07df924b203a35bffbbf68d46442353f73af
dunespace <dunespace@icloud.com>
2024-09-16 14:00:01 +0200

    Improve contract auditing tools

--- proposal_handler.rs
+++ proposal_handler.rs
@@ -5,3 +5,4 @@
 // Optimize gas usage in smart contracts
 // Improve gas fee estimation accuracy
 // Add support for multi-signature wallet contracts
+// Add support for multiple consensus mechanisms
