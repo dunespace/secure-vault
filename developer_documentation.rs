67c72dc642efa988c03c9e57cb7533d897f9d2a9
dunespace <dunespace@icloud.com>
2024-12-26 14:00:00 +0200

    Fix issue in token vesting logic

--- developer_documentation.rs
+++ developer_documentation.rs
@@ -7,3 +7,4 @@
 // Optimize storage for off-chain data
 // Fix security vulnerability in random number generation
 // Improve security in decentralized applications
+// Optimize gas usage in smart contracts
